c98efcaa13348bf53de3ab72526a559e479768ba
Judi Caplan <Jude@MacBook.local>
2019-04-15 01:14:17 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -1,6 +1,36 @@
+var cards = [
+{rank: "queen",
+suit: "hearts",
+cardImage: "images/queen-of-hearts.png"},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 
-var cards = ["queen", "queen", "king", "king"]
+var createBoard = function(){
+	for (i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i)
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
 var cardsInPlay = []
+	
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
@@ -10,19 +40,12 @@ var checkForMatch = function(){
 }
 
 var flipCard = function(cardId){
-	var cardOne = []
-	var cardTwo = [] 
-
-	console.log("User flipped " + cards[cardId]);
-    cardsInPlay.push(cards[cardId]);
-
-
-    }
-
-flipCard(0);
-flipCard(2);
-checkForMatch();
-	
-	
-
-
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
+    cardsInPlay.push(cards[cardId].rank);
+	if (cardsInPlay.length === 2){
+		checkForMatch();
+}
+}
+    
+createBoard();
