ed924d67c7431bf2df3e6d920f01fb666820a037
Judi Caplan <Jude@MacBook.local>
2019-04-15 10:37:07 -0400

    created game logic

--- memory_game/JS/main.js
+++ memory_game/JS/main.js
@@ -30,22 +30,44 @@ var createBoard = function(){
 }
 
 var cardsInPlay = []
-	
+
+var gamesPlayed = 0
+var gamesWon = 0
+
 var checkForMatch = function(){
 	if (cardsInPlay[0] === cardsInPlay[1]){
 		alert("You found a match!");
+		gamesPlayed++
+		gamesWon++
 	}else{
 		alert("Sorry, try again");
+		gamesPlayed++
 	}
 }
 
-var flipCard = function(cardId){
+var flipCard = function(){
 	var cardId = this.getAttribute('data-id');
 	this.setAttribute('src', cards[cardId].cardImage);
     cardsInPlay.push(cards[cardId].rank);
 	if (cardsInPlay.length === 2){
-		checkForMatch();
+		setTimeout(checkForMatch, 60);
+		setTimeout(newGame, 80);
 }
 }
     
 createBoard();
+
+var newGame = function(){
+	alert("Games won: " + gamesWon + "\nGames played: " + gamesPlayed);
+   	if (confirm("Would you like to play again?")){
+		cardsInPlay.length = 0;
+		 var card = document.getElementsByTagName('img');
+    	for (i = 0; i < cards.length; i++) {
+        	if (card[i].getAttribute('src') != 'images/back.png') {
+            	card[i].setAttribute('src', 'images/back.png');
+        }
+    }
+	}else{
+		alert("Thank you for playing. Here is your final score:" + "\nGames won: " + gamesWon + "\nGames played: " + gamesPlayed);
+	}
+}
